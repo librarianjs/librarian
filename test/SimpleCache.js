1f3eba1412dd907f69c490730e59d6120ced72fb
Josh Vanderwillik <joshwillik@gmail.com>
2017-12-21 13:59:28 -0500

    Use async functions

--- test/SimpleCache.js
+++ test/SimpleCache.js
@@ -4,15 +4,15 @@ module.exports = class SimpleCache {
     this._put = 0
     this._hit = 0
   }
-  get (key) {
+  async get (key) {
     let value = this.cache[key] || null
     if (value) {
       this._hit += 1
     }
-    return Promise.resolve(value)
+    return value
   }
-  put (key, value) {
+  async put (key, value) {
     this._put += 1
-    return Promise.resolve(this.cache[key] = value)
+    return this.cache[key] = value
   }
 }
